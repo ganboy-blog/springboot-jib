f51e2e38b907c1220859b441cc05b3cb929fafa7
wangshu <kingwsi@icloud.com>
2019-11-01 17:16:33 +0800

    初始化提交

---
+++ src/test/java/com/ws/docker/DockerDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ws.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
